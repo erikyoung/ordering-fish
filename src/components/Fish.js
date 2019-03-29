5771e426635c5f49166be633fe8643a6b9efe1d8
erikyoung <erik.young@rmit.edu.vn>
2019-03-29 16:25:12 +0700

    adding sold out or available to the buttons

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -10,13 +10,17 @@ export default class Fish extends Component {
   	const name = this.props.details.name;
   	const price = this.props.details.price;
   	const desc = this.props.details.desc; 
+  	const status = this.props.details.status; 
+  	const isAvailable = status === 'available'; 
     return (
       <li className="menu-fish">
-     		<img src={image} alt="fuck" />
+     		<img src={image} alt="sample photo" />
      		<h3 className="fish-name">
      		{name}
      			<span className="price">{formatPrice(price)}</span>
      		</h3>
+     		<p>{desc}</p>
+     		<button disabled={!isAvailable}>{isAvailable ? 'Add to Order' : 'Sold Out'}Add To Cart</button>
       </li>
     );
   }
